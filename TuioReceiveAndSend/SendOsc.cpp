ae0fe0fbaf08e18d0bd1c0d3dc9e8e614f54dc8a
Issei <menmaman53@gmail.com>
2023-03-28 19:21:46 +0900

    Release Version

--- TuioReceiveAndSend/SendOsc.cpp
+++ TuioReceiveAndSend/SendOsc.cpp
@@ -1,12 +1,11 @@
 /**
  * @file SendOsc.cpp
- * @author Issei Furutani (fullonefine@gmail.com)
- * @brief
+ * @author Issei Furutani
  * このプログラムは加工されたTUIOのメッセージをOSCのメッセージとして、送信するプログラムです。
  * 送られるOSCのメッセージは以下の内容である。
  * /fillter/tuio add SessionID X Y
  * /fillter/tuio update SessionID X Y
- * /fillter/tuio remove SessionID
+ * /fillter/tuio remove SessionID X Y
  * @version 1.0
  * @date 2022-11-08
  */
@@ -15,13 +14,16 @@
 
 #include "ReceiveTuio.h"
 
-const char* send_to_address = "192.168.1.194";
+// 送信先のIPアドレスとポート番号
+const char *send_to_address = "127.0.0.1";
 const int send_to_port = 50000;
 static int output_buffer_size = 1024;
 
-namespace oscMessage {
+namespace oscMessage
+{
 
-void SendOsc::SendMessage(const char* state, int SessionID) {
+  void SendOsc::SendMessage(const char *state, int SessionID)
+  {
     UdpTransmitSocket transmitSocket(
         IpEndpointName(send_to_address, send_to_port));
 
@@ -32,10 +34,11 @@ void SendOsc::SendMessage(const char* state, int SessionID) {
       << state << SessionID << osc::EndMessage << osc::EndBundle;
 
     transmitSocket.Send(p.Data(), p.Size());
-}
+  }
 
-void SendOsc::SendMessageWithPosition(const char* state, int SessionID, float x,
-                                      float y) {
+  void SendOsc::SendMessageWithPosition(const char *state, int SessionID, float x,
+                                        float y)
+  {
     UdpTransmitSocket transmitSocket(
         IpEndpointName(send_to_address, send_to_port));
 
@@ -46,6 +49,6 @@ void SendOsc::SendMessageWithPosition(const char* state, int SessionID, float x,
       << state << SessionID << x << y << osc::EndMessage << osc::EndBundle;
 
     transmitSocket.Send(p.Data(), p.Size());
-}
+  }
 
-}  // namespace oscMessage
+} // namespace oscMessage
